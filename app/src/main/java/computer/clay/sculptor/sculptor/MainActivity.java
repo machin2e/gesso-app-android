7085ddfec3ce4989da5736995d90110f5a5acc2e
Michael Gubbels <mrgubbels@gmail.com>
2015-11-14 16:56:33 -0600

    Added UDP dispatcher capable of sending and receiving datagrams.

--- app/src/main/java/computer/clay/sculptor/sculptor/MainActivity.java
+++ app/src/main/java/computer/clay/sculptor/sculptor/MainActivity.java
@@ -3,8 +3,13 @@ package computer.clay.sculptor.sculptor;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,6 +17,10 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        textMessage = (TextView) findViewById(R.id.messageText);
+//        runServer = new RunServerInThread();
+//        runServer.start();
     }
 
 
@@ -40,4 +49,84 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private static final int UDP_SERVER_PORT = 4445;
+    private static final int MAX_UDP_DATAGRAM_LEN = 1500;
+    private TextView textMessage;
+    private MyDatagramReceiver myDatagramReceiver = null;
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        myDatagramReceiver.kill();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        myDatagramReceiver = new MyDatagramReceiver();
+        myDatagramReceiver.start();
+    }
+
+    /**
+     * UDP Server
+     */
+    private class MyDatagramReceiver extends Thread {
+        private boolean bKeepRunning = true;
+        private String lastMessage = "";
+
+        public void run() {
+            String message;
+            byte[] lmessage = new byte[MAX_UDP_DATAGRAM_LEN];
+            DatagramPacket packet = new DatagramPacket(lmessage, lmessage.length);
+
+            DatagramSocket socket = null;
+            try {
+                socket = new DatagramSocket(UDP_SERVER_PORT);
+
+                while(bKeepRunning) {
+                    socket.receive(packet);
+                    message = new String(lmessage, 0, packet.getLength());
+                    lastMessage = message;
+                    runOnUiThread(updateTextMessage);
+                }
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+
+            if (socket != null) {
+                socket.close();
+            }
+        }
+
+        public void kill() {
+            bKeepRunning = false;
+        }
+
+        public String getLastMessage() {
+            return lastMessage;
+        }
+
+        // Protocaller
+        // Transmitter
+        // Protocolist
+        // Relay
+        // Router
+        // Networking
+        // Socializer
+        // Synchronizer
+    }
+
+    private Runnable updateTextMessage = new Runnable() {
+        public void run() {
+            if (myDatagramReceiver == null) return;
+//            textMessage.setText(myDatagramReceiver.getLastMessage());
+
+//            String httpRequestText = httpRequestAdapter.getItem(position); //CharSequence text = "Hello toast!";
+//                int duration = Toast.LENGTH_SHORT;
+//            Toast toast = Toast.makeText(getParent(), myDatagramReceiver.getLastMessage(), Toast.LENGTH_SHORT); //Toast toast = Toast.makeText(context, text, duration);
+//            toast.show();
+            Log.v("Messenger", myDatagramReceiver.getLastMessage());
+        }
+    };
 }
