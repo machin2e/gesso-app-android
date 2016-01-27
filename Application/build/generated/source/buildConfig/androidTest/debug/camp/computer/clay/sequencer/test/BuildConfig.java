d2fabf68e1ce42e82135c04c3a3b4185191bb326
Michael Gubbels <mrgubbels@gmail.com>
2016-01-26 19:36:55 -0800

    Renamed project to Sequencer.

---
+++ Application/build/generated/source/buildConfig/androidTest/debug/camp/computer/clay/sequencer/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package camp.computer.clay.sequencer.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "camp.computer.clay.sequencer.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
