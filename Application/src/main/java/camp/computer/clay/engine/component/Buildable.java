0104eedb45e597ba5cbf4689805c24225165b2c1
Michael Gubbels <Michael Gubbels>
2016-12-06 17:45:04 -0800

    Checkpoint. Impending major cleanup!

--- Application/src/main/java/camp/computer/clay/engine/component/Buildable.java
+++ Application/src/main/java/camp/computer/clay/engine/component/Buildable.java
@@ -1,10 +1,10 @@
 package camp.computer.clay.engine.component;
 
-public class Assemblable extends Component {
+public class Buildable extends Component {
 
     // TODO: List of assembly instructions and assembly state for the Entity's Extension and connected Host.
 
-    public Assemblable() {
+    public Buildable() {
         super();
     }
 }
